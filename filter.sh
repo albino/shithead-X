dcf51daefee6a77f6cd768da7a2c9fd60d6b4941
Al Beano <albino@autistici.org>
2020-05-20 22:00:22 +0100

    Filter improvements

--- filter.sh
+++ filter.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Quick and dirty filter to remove "bad" lines
-sed '/\[YouTube\]/d' | sed '/\[4[Cc]han\]/d' | sed '/https\?:\/\//d'
+sed '/\[YouTube\]/d' |
+sed '/\[4[Cc]han\]/d' |
+sed '/https\?:\/\//d' |
+sed '/^\./d' |
+sed '/^s\//d'
